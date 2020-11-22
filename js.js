0069ec8163a833c18d0cb98e8834db31a4c695dc
woutcorijn <26119233+woutcorijn@users.noreply.github.com>
2020-11-22 13:43:46 +0100

    V0.04

--- js.js
+++ js.js
@@ -1,17 +1,40 @@
-	function readURL(input) {
+var height = 0;
+var loopid
+
+function readURL(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
             reader.onload = function (e) {
                 $('#img')
                     .attr('src', e.target.result)
-                    .width(0)
-                    .height(0);
+                    .height(0)
+										.width(0);
             };
 
             reader.readAsDataURL(input.files[0]);
         }
+				animation()
     }
-	function ani(){
-            document.getElementById('img').className ='classname';
-        }	
+
+	function animation(){
+					  document.getElementById('audio').play();
+            // document.getElementById('img').className ='animation';
+						loopid = setInterval(loop,100);
+        }
+
+function loop() {
+	height++;
+	document.getElementById('img').style.height = height.toString() + "px";
+	document.getElementById('img').style.width = "auto";
+	console.log(document.getElementById("img").style.height);
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    if( height >= 150) {
+    clearInterval(loopid);
+      }
+	} else {
+    if( height >= 300) {
+    clearInterval(loopid);
+      }
+	}
+}
